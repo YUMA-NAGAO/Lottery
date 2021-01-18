d003a219d691db2b35ec7951b325e87dc3195a6d
YUMA-NAGAO <55423883+YUMA-NAGAO@users.noreply.github.com>
2021-01-18 11:01:55 +0900

    [edit]シンプルなフォームの作成

--- Lottery/Lottery/Form1.Designer.cs
+++ Lottery/Lottery/Form1.Designer.cs
@@ -29,13 +29,79 @@ namespace Lottery
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F);
+            this.button1.Location = new System.Drawing.Point(454, 307);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(334, 77);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "抽選スタート！！！";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.checkBox1.Location = new System.Drawing.Point(314, 349);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(114, 35);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "誕生日";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.checkBox2.Location = new System.Drawing.Point(314, 308);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(87, 35);
+            this.checkBox2.TabIndex = 2;
+            this.checkBox2.Text = "通常";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F);
+            this.label1.Location = new System.Drawing.Point(208, 116);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(301, 76);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "抽選結果";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Lottery/Lottery/Form1.cs
+++ Lottery/Lottery/Form1.cs
@@ -16,5 +16,20 @@ namespace Lottery
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
