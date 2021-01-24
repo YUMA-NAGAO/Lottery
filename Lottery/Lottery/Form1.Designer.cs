cd67593502709c288e27e7e76651e3f946b74268
YUMA-NAGAO <55423883+YUMA-NAGAO@users.noreply.github.com>
2021-01-25 08:59:55 +0900

    [edit]フォルダの整理

--- Lottery/Lottery/Form1.Designer.cs
+++ Lottery/Lottery/Form1.Designer.cs
@@ -29,28 +29,28 @@ namespace Lottery
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.GatyaButton = new System.Windows.Forms.Button();
             this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.checkBox2 = new System.Windows.Forms.CheckBox();
             this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // button1
+            // GatyaButton
             // 
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F);
-            this.button1.Location = new System.Drawing.Point(454, 307);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(334, 77);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "抽選スタート！！！";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.GatyaButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F);
+            this.GatyaButton.Location = new System.Drawing.Point(182, 263);
+            this.GatyaButton.Name = "GatyaButton";
+            this.GatyaButton.Size = new System.Drawing.Size(380, 55);
+            this.GatyaButton.TabIndex = 0;
+            this.GatyaButton.Text = "抽選スタート！！！";
+            this.GatyaButton.UseVisualStyleBackColor = true;
+            this.GatyaButton.Click += new System.EventHandler(this.GatyaButton);
             // 
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
             this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
-            this.checkBox1.Location = new System.Drawing.Point(314, 349);
+            this.checkBox1.Location = new System.Drawing.Point(28, 304);
             this.checkBox1.Name = "checkBox1";
             this.checkBox1.Size = new System.Drawing.Size(114, 35);
             this.checkBox1.TabIndex = 1;
@@ -62,7 +62,7 @@ namespace Lottery
             // 
             this.checkBox2.AutoSize = true;
             this.checkBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
-            this.checkBox2.Location = new System.Drawing.Point(314, 308);
+            this.checkBox2.Location = new System.Drawing.Point(28, 263);
             this.checkBox2.Name = "checkBox2";
             this.checkBox2.Size = new System.Drawing.Size(87, 35);
             this.checkBox2.TabIndex = 2;
@@ -74,23 +74,25 @@ namespace Lottery
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F);
-            this.label1.Location = new System.Drawing.Point(208, 116);
+            this.label1.Location = new System.Drawing.Point(65, 89);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(301, 76);
             this.label1.TabIndex = 3;
             this.label1.Text = "抽選結果";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(581, 352);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.checkBox2);
             this.Controls.Add(this.checkBox1);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.GatyaButton);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -98,7 +100,7 @@ namespace Lottery
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button GatyaButton;
         private System.Windows.Forms.CheckBox checkBox1;
         private System.Windows.Forms.CheckBox checkBox2;
         private System.Windows.Forms.Label label1;
--- Lottery/Lottery/Form1.cs
+++ Lottery/Lottery/Form1.cs
@@ -17,17 +17,22 @@ namespace Lottery
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void Form1_Load(object sender, EventArgs e)
         {
 
         }
 
-        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        private void Birthday(object sender, EventArgs e)
         {
 
         }
 
-        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        private void Nomal(object sender, EventArgs e)
+        {
+
+        }
+
+        private void GatyaButtonn(object sender, EventArgs e)
         {
 
         }
